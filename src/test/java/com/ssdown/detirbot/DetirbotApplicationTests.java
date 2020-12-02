a5dd76566e9c9fcc17412eb4c8509ed55d7a2a9c
Toneberry <sheepserverdown@gmail.com>
2020-12-02 15:02:47 +0900

    Initial commit

---
+++ src/test/java/com/ssdown/detirbot/DetirbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssdown.detirbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetirbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
