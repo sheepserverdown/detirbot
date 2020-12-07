417f1b5629ff88bea300bba5692c01d59b44a941
Toneberry <sheepserverdown@gmail.com>
2020-12-07 22:22:21 +0900

    기본 상수 클래스(Constants) 작성

---
+++ src/main/java/com/ssdown/detirbot/Constants.java
@@ -0,0 +1,10 @@
+package com.ssdown.detirbot;
+
+public class Constants {
+    public static final int EXIT_CODE_NORMAL = 0;
+    public static final int EXIT_CODE_ERROR = 1;
+    public static final int EXIT_CODE_RESTART = 10;
+    public static final int EXIT_CODE_UPDATE = 11;
+
+    public static final String propertyDirectory = "resources/application.properties";
+}
