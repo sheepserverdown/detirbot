00b95346c5d57d17cff45f91cfd662c6a3b246c0
Toneberry <sheepserverdown@gmail.com>
2020-12-09 00:52:04 +0900

    설정 파일 클래스 update

--- src/main/java/com/ssdown/detirbot/config/Configuration.java
+++ src/main/java/com/ssdown/detirbot/config/Configuration.java
@@ -1,22 +1,39 @@
 package com.ssdown.detirbot.config;
 
+import com.ssdown.detirbot.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Properties;
 
-public class Configuration {
+public class Configuration extends PropertyConfiguration {
     public static final Logger log = LoggerFactory.getLogger(Configuration.class);
 
-    private final String fileName;
-    private File folder;
+    private static Configuration instance;
 
-    //생성자 초기화
-    public Configuration(File folder, String fileName) {
-        this.folder = folder;
-        this.fileName = fileName;
+    private File configFile;
+    public final String privateToken;
+
+    public Configuration() {
+        this.configFile = new File(Constants.propertyDirectory, Constants.propertyFilename);
+
+        getProperty(getClass().getClassLoader(), Constants.propertyFilename);
+
+        this.privateToken = properties.getProperty("token");
     }
 
+    //인스턴스 생성 후 값을 받아오기 위함
+    public static Configuration getInstance() {
+        if(instance == null) {
+            instance = new Configuration();
+        }
 
+        return instance;
+    }
+
+    //파일 존재 여부 확인
+    public boolean exists() {
+        return configFile != null && configFile.exists() && configFile.isFile();
+    }
 }
