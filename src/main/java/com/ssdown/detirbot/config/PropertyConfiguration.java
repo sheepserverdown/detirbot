a33a8ec66b8e9b643f6d4c1225d126dd903869b1
sheepserverdown <sheepserverdown@gmail.com>
2021-03-01 18:48:23 +0900

    update) .properties 파일 경로 취득

--- src/main/java/com/ssdown/detirbot/config/PropertyConfiguration.java
+++ src/main/java/com/ssdown/detirbot/config/PropertyConfiguration.java
@@ -1,8 +1,11 @@
 package com.ssdown.detirbot.config;
 
+import com.ssdown.detirbot.Constants;
 import com.ssdown.detirbot.exception.FailedLoadPropertyException;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 
@@ -17,6 +20,13 @@ public class PropertyConfiguration {
         } catch (IOException e) {
             throw new FailedLoadPropertyException(fileName + "파일을 불러오는데 실패하였습니다.", e);
         }
+   }
+
+   protected File getFile() {
+       String filename = Constants.propertyFilename;
+       URL url = PropertyConfiguration.class.getClassLoader().getResource(filename);
+       File file = new File(url.toString().substring(6,url.toString().length() - filename.length()), filename);
+       return file;
    }
 
 
