a9a1657ee8c37e2266047e4b80294dcef3ecdd67
sheepserverdown <sheepserverdown@gmail.com>
2021-03-30 21:39:43 +0900

    delete) spring boot 잔재 삭제 당장은 Spring Boot를 활용할 것 같지는 않아 일단 지워둔다.

--- src/main/java/com/ssdown/detirbot/Main.java
+++ src/main/java/com/ssdown/detirbot/Main.java
@@ -1,15 +1,7 @@
 package com.ssdown.detirbot;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 public class Main {
 	public static void main(String[] args) {
-		//Spring Boot 이용시 진입
-		//SpringApplication.run(MainBootstrap.class, args);
-
-		//프레임워크 안쓰는 구동
-
 		try {
 			DetirBot.detirBot = new DetirBot();
 		} catch(Exception e) {
