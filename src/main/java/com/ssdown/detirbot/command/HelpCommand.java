38e1e28de7a958ee1cc4af3a93eb9801e5a97d7c
sheepserverdown <sheepserverdown@gmail.com>
2021-03-30 21:38:58 +0900

    test) 커맨드 test 중.. Thread 문제를 해결했다. interrupt는 먹히지 않아서 run()내부에 있는 while의 조건을 변경하는 것으로 유사 interrupt를 적용 사실상 이게 Thread를 죽이는 식이라곤 하지만 JVM이 이걸 자동으로 정리해주는지는 더 찾아봐야 할것으로 보인다.

--- src/main/java/com/ssdown/detirbot/command/HelpCommand.java
+++ src/main/java/com/ssdown/detirbot/command/HelpCommand.java
@@ -1,5 +1,7 @@
 package com.ssdown.detirbot.command;
 
+import com.ssdown.detirbot.DetirBot;
+import lombok.Setter;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
@@ -8,40 +10,74 @@ import java.util.Collections;
 import java.util.List;
 
 public class HelpCommand extends Command {
+    private HandlerClass handlerClass = new HandlerClass(DetirBot.getInstance());
+
     @Override
     public void executeCommand(String[] args, MessageReceivedEvent e, MessageSender chat) {
         Guild guild = e.getGuild();
+        handlerClass.setChat(chat);
 
-//        chat.sendMessage(Arrays.toString(args));
+        if(e.getMember().getId().equals("")) {
+            chat.sendMessage("");
+        } else {
+            chat.sendMessage("권한이 부족합니다.");
+        }
 
-        Thread thread = new Thread(new Runnable() {
-            int count = 0;
-            @Override
-            public void run() {
-                while(true) {
-                    chat.sendMessage(Integer.toString(count));
-                    count++;
-                    try {
-                       Thread.sleep(1000);
-                    } catch(Exception ex) {
-                        ex.printStackTrace();
+        if(args.length > 0) {
+            switch(args[0].toLowerCase()) {
+                case "start":
+                    if (handlerClass.stop) {
+                        handlerClass.interrupt();
+                        handlerClass = new HandlerClass(chat);
                     }
-                }
+                    handlerClass.start();
+                    break;
+                case "stop":
+//                handlerClass.interrupt();
+                    handlerClass.setStop(true);
+                    break;
+                case "state":
+                    chat.sendMessage("stop : " + handlerClass.stop);
+                    break;
+                default:
+                    break;
             }
-        });
-
-        switch(args[0].toLowerCase()) {
-            case "start":
-                thread.start();
-                break;
-            case "stop":
-                thread.interrupt();
-                break;
         }
     }
 
     @Override
     public List<String> getAlias() {
         return Collections.singletonList("help");
+    }
+
+    @Setter
+    class HandlerClass extends Thread {
+        private MessageSender chat;
+        private DetirBot detirBot;
+        private boolean stop = false;
+
+        public HandlerClass() {}
+
+        public HandlerClass (MessageSender chat) {
+            this.chat = chat;
+        }
+
+        public HandlerClass (DetirBot detirBot) {
+            this.detirBot = detirBot;
+        }
+
+        @Override
+        public void run() {
+            int count = 0;
+            while(!stop) {
+                try {
+                    count++;
+                    Thread.sleep(2000);
+                } catch(InterruptedException ex) {
+                    ex.printStackTrace();
+                    chat.sendMessage("스레드 멈췄음 : " + stop);
+                }
+            }
+        }
     }
 }
